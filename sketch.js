2097f7d08ca965629ca40276e4e6f35e475734b5
Glitch (scratch-general-mistake) <none>
2021-05-27 02:24:38 +0000

    🌗🚓 Checkpoint ./sketch.js:3856783/1558

--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
-let prevX = -1;
-let prevY = -1;
+let prevX;
+let prevY;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  stroke(255);
-  line(prevX, prevY, mouseX, mouseY);
+  stroke(25);
 }
 
 function mousePressed() {
+  for (let i = 0; i < 3; i++) {
+    line(
+      prevX + random(-10, 10),
+      prevY + random(-10, 10),
+      mouseX + random(-10, 10),
+      mouseY + random(-10, 10)
+    );
+  }
   prevX = mouseX;
   prevY = mouseY;
 }
