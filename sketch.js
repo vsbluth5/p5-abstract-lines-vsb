aa3ae297fd43c24af4e3526bd7e936a908ab0f0c
Glitch (scratch-general-mistake) <none>
2021-05-27 02:58:07 +0000

    🏏🍱 Checkpoint ./sketch.js:3856783/1179

--- sketch.js
+++ sketch.js
@@ -1,36 +1,24 @@
-let x1 = windowWidth/2;
-let x2 = windowWidth/2+5;
-let y1 = windowHeight/2;
-let y2 = windowHeight/2;
+let x1 = windowWidth / 2;
+let x2 = windowWidth / 2 + 5;
+let y1 = windowHeight / 2;
+let y2 = windowHeight / 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  for (let i = 0; i < 3; i++) {
-    strokeWeight(random(2,5))
-    stroke(
-      random(x2 % 255),
-      random(y2 % 255),
-      random((windowWidth - x2) % 255)
-    );
-    line(
-      x1 + random(-10, 10),
-      y1 + random(-10, 10),
-      x2 + random(-10, 10),
-      y2 + random(-10, 10)
-    );
-  } // end of fow-loop
-  
+  textSize(32);
+  text(x1+", "+y1+", "+x2+", "+y2, 20, 20)
+  strokeWeight(random(4, 7));
+  stroke(25);
+  line(x1, y1, x2, y2);
+
   x1 = x2;
   y1 = y2;
-  
-  x2 = random(windowWidth)
-  y2 = random(windowHeight)
+
+  x2 = random(windowWidth);
+  y2 = random(windowHeight);
 }
 
-
-function mousePressed() {
-  
-}
+function mousePressed() {}
